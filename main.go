056e20eb368005c6a4778cb588b2df04324aa531
bpc109 <wpesfriendnva@proton.me>
2024-01-24 13:21:24 -0500

    Updated main.go to remove dependacy on fmt

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"backend/models"
 	"backend/routers"
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -16,7 +15,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println("DB Connected")
+	log.Println("DB Connected")
 	models.MakeBook(db)
 
 	BookRouter := &routers.BookRouter{DB: db}
@@ -24,6 +23,6 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/api/books/", BookRouter.HandleBooks)
 
-	fmt.Println("Listening on :8080")
+	log.Println("Listening on :8080")
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
